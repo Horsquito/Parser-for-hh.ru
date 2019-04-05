8d78c879e15cdd6b98b123746727dc8415ec8a81
Alex <a.zemisoff@gmail.com>
2019-04-05 11:23:49 +0300

    improved parser for hh.ru

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import csv
 
 
 headers = {'accept': '*/*',
@@ -9,26 +10,53 @@ base_url = 'https://hh.ru/search/vacancy?text=python&area=1&page=0'
 
 def hh_parse(base_url, headers):
     jobs = []
+    urls = []
+    urls.append(base_url)
     session = requests.Session()
     request = session.get(base_url, headers=headers)
     if request.status_code == 200:
-        soup = bs(request.content, 'html.parser')
-        divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
-        for div in divs:
-            title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
-            href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
-            company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-            text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
-            text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
-            content = text1 + ' ' + text2
-            jobs.append({
-                'title': title,
-                'company': company,
-                'href': href,
-                'content': content
-                })
+        soup = bs(request.content, 'lxml')
+        try:
+            pagination = soup.find_all('a', attrs={'data-qa': 'pager-page'})
+            count = int(pagination[-1].text)
+            for i in range(count):
+                url = f'https://hh.ru/search/vacancy?text=python&area=1&page={i}'
+                if url not in urls:
+                    urls.append(url)
+        except:
+            pass
+        for url in urls:
+            request = session.get(url, headers=headers)
+            soup = bs(request.content, 'lxml')
+            divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
+            for div in divs:
+                try:
+                    title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+                    href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
+                    company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                    text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+                    text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+                    content = text1 + ' ' + text2
+                    jobs.append({
+                        'title': title,
+                        'company': company,
+                        'href': href,
+                        'content': content
+                        })
+                except:
+                    pass
             print(len(jobs))
-        else:
-            print('ERROR')
+    else:
+        print('ERROR')
+    return jobs
 
-hh_parse(base_url, headers)
+def files_writer(jobs):
+    with open('parser_jobs.csv', 'w') as file:
+        a_pen = csv.writer(file)
+        a_pen.writerow(('Название вакансии', 'URL', 'Название компании', 'Описание'))
+        for job in jobs:
+            a_pen.writerow((job['title'], job['href'], job['company'], job['content']))
+
+
+jobs = hh_parse(base_url, headers)
+files_writer(jobs)
